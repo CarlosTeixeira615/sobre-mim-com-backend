3726575ee52016550f533b006d580bb410885fe3
CarlosTeixeira.silva <carlosteixeira.tc@hotmail.com>
2020-05-22 11:28:14 -0300

    ..

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-const express= require('express')
-const nunjucks = require('nunjucks')
+const express = require('express');
+const nunjucks = require('nunjucks');
 
-const server = express()
-server.use(express.static('public'))
+const server = express();
+server.use(express.static('public'));
 
-server.set("view engine", "html")
-nunjucks.configure("views", {
-  express: server
-})
-
-server.get("/", function(req,res){
-  return res.render("index")
+server.set('view engine', 'html');
+nunjucks.configure('views', {
+	express: server
 });
 
-server.listen(7000, function(){
-  console.log("server is online")
+server.get('/', function(req, res) {
+	return res.render('index');
+});
+
+server.listen(5000, function() {
+	console.log('server is online');
 });
